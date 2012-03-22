4990b19461c3ea7dc33fb8dab9aab341095a7b06
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-03-22 21:46:37 +0200

    some code clean up and user interface improvement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,4 @@
 
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-tab
--- app/controllers/wikipages_controller.rb
+++ app/controllers/wikipages_controller.rb
@@ -18,4 +18,19 @@ class WikipagesController < ApplicationController
       end
     end   
   end
+  
+  def edit
+    @wikipage = Wikipage.find(params[:id])    
+  end
+  
+  def update
+    @wikipage = Wikipage.find(params[:id])
+    @wikipage.update_attributes(params[:wikipage])
+    
+    if @wikipage.save
+      redirect_to @wikipage, notice: "Wikipage successfully edited"
+    end
+    
+  end
+  
 end
