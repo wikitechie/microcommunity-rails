861b4d4019ca07218a2f5fd57ce80b6a780d4b9d
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-03-24 13:58:55 +0200

    refactored wikipages controller and views

--- app/controllers/wikipages_controller.rb
+++ app/controllers/wikipages_controller.rb
@@ -1,40 +1,83 @@
 class WikipagesController < ApplicationController
+  # GET /wikipages
+  # GET /wikipages.json
+  def index
+    @wikipages = Wikipage.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @wikipages }
+    end
+  end
+
+  # GET /wikipages/1
+  # GET /wikipages/1.json
+  def show
+    @wikipage = Wikipage.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @wikipage }
+    end
+  end
+
+  # GET /wikipages/new
+  # GET /wikipages/new.json
   def new
     @wikipage = Wikipage.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @wikipage }
+    end
   end
-  
-  def index
-    @wikipages = Wikipage.find(:all, :order => "updated_at ASC")
+
+  # GET /wikipages/1/edit
+  def edit
+    @wikipage = Wikipage.find(params[:id])
   end
-  
-  def show
-    @wikipage = Wikipage.find(params[:id])    
-  end
-  
+
+  # POST /wikipages
+  # POST /wikipages.json
   def create
-    @user = current_user
-    @wikipage = @user.wikipages.create!(params[:wikipage]) 
-    
+    @wikipage = Wikipage.new(params[:wikipage])
+
     respond_to do |format|
       if @wikipage.save
-        format.html { redirect_to @wikipage, notice: 'Post was successfully created.' }
-        format.js
+        format.html { redirect_to @wikipage, notice: 'Wikipage was successfully created.' }
+        format.json { render json: @wikipage, status: :created, location: @wikipage }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @wikipage.errors, status: :unprocessable_entity }
       end
-    end   
+    end
   end
-  
-  def edit
-    @wikipage = Wikipage.find(params[:id])    
-  end
-  
+
+  # PUT /wikipages/1
+  # PUT /wikipages/1.json
   def update
     @wikipage = Wikipage.find(params[:id])
-    @wikipage.update_attributes(params[:wikipage])
-    
-    if @wikipage.save
-      redirect_to @wikipage, notice: "Wikipage successfully edited"
+
+    respond_to do |format|
+      if @wikipage.update_attributes(params[:wikipage])
+        format.html { redirect_to @wikipage, notice: 'Wikipage was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @wikipage.errors, status: :unprocessable_entity }
+      end
     end
-    
   end
-  
+
+  # DELETE /wikipages/1
+  # DELETE /wikipages/1.json
+  def destroy
+    @wikipage = Wikipage.find(params[:id])
+    @wikipage.destroy
+
+    respond_to do |format|
+      format.html { redirect_to wikipages_url }
+      format.json { head :ok }
+    end
+  end
 end
