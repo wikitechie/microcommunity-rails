3e3325c6bd7bf9c1111e8d0ec4ad94a4aead5597
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-03-25 22:04:28 +0200

    making a welcome page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,25 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!
-  def home    
-    @wikipage = current_user.wikipages.new
+  
+  def home
     
-    #activites of followed users
-    @following_users = current_user.following_users
-    @following_users_ids = @following_users.map  do |user|
-      user.id
+    if current_user
+      #instance variable used to initialize the form of the composer
+      @wikipage = current_user.wikipages.new
+      
+      #fetching activites of followed users
+      @following_users = current_user.following_users
+      @following_users_ids = @following_users.map  do |user|
+        user.id
+      end
+      @activities = Activity.where(:user_id => @following_users_ids)
+      
+      render "home"
+      
+    else
+      render "welcome"
     end
-    @activities = Activity.where(:user_id => @following_users_ids)
+          
+
     
     
   end
