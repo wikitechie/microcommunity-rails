12e601ad585a97f297eae485c959ac800166bf59
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2012-03-28 18:42:22 +0200

    removed attendants model/controller

--- app/models/attendant.rb
+++
@@ -1,4 +0,0 @@
-class Attendant < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :event
-end
--- spec/factories/attendants.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :attendant do
-    user_id 1
-    event_id 1
-  end
-end
