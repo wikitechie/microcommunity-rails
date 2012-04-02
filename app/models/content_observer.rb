5ef2eb36ad6f520d1a953d89c510e04c46b9e539
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-04-02 12:23:10 +0200

    support for observing edit action

--- app/models/content_observer.rb
+++ app/models/content_observer.rb
@@ -11,5 +11,15 @@ class ContentObserver < ActiveRecord::Observer
       :updated_at => action_object.updated_at)
   end
 
+  def after_update(action_object)
+    Activity.create!(
+      :user_id => User.current_user.id,
+      :action_object_id => action_object.id,
+      :action_object_type => action_object.class.to_s,
+      :verb => "edit",
+      :created_at => action_object.created_at,
+      :updated_at => action_object.updated_at)
+  end
+
 end
 
