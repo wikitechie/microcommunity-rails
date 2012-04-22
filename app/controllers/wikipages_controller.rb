0cdc2616ec565eeb9a07e496d078e1639272150c
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-04-22 09:33:11 +0300

    loading wiki pages inside the group home page

--- app/controllers/wikipages_controller.rb
+++ app/controllers/wikipages_controller.rb
@@ -18,6 +18,7 @@ class WikipagesController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @wikipage }
+      format.js
     end
   end
 
